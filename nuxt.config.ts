752172e59421a39996cc1d8324a2fddd20f6419b
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-06-30 00:10:20 +0200

    eslint setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
+// Modules
 import { defineNuxtConfig } from 'nuxt'
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	typescript: {
 		strict: true
+	},
+	vite: {
+		plugins: [
+			eslintPlugin()
+		]
 	}
 })
