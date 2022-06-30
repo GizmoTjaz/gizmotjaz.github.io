1ce46c859d2a5b8d3b8c4c446f37690014c3e619
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-06-30 23:53:02 +0200

    profile avatar component

---
+++ components/ProfileAvatar.vue
@@ -0,0 +1,34 @@
+<template>
+	<img
+		class="profile-avatar"
+		:src="url"
+		alt="Profile picture"
+	>
+</template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "ProfileAvatar",
+		props: {
+			url: {
+				type: String,
+				required: true
+			}
+		}
+	});
+
+</script>
+
+<style lang="scss" scoped>
+
+	.profile-avatar {
+		width: 200px;
+		height: 200px;
+		border-radius: 50%;
+	}
+
+</style>
