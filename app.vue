c4ddf1b5a3ca7aa279c3c3921f263cbeabb9a194
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-06-30 21:02:35 +0200

    fetch avatar & username

--- app.vue
+++ app.vue
@@ -1,5 +1,44 @@
 <template>
 	<div>
-		<NuxtWelcome />
+		<section v-if="name && avatarUrl">
+			<ProfileAvatar :src="avatarUrl" />
+			<h1>{{ name }}</h1>
+		</section>
 	</div>
 </template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	// Local Components
+	import ProfileAvatar from "@components/ProfileAvatar.vue";
+
+	// Types
+	interface GitHubResponse {
+		name: string;
+		avatar_url: string;
+	}
+
+	export default defineComponent({
+		name: "App",
+		components: {
+			ProfileAvatar
+		},
+		data () {
+			return {
+				name: null as string | null,
+				avatarUrl: null as string | null
+			};
+		},
+		async mounted () {
+
+			const res = await $fetch<GitHubResponse>(`https://api.github.com/users/${$nuxt.$config.public.GITHUB_USERNAME}`);
+
+			this.name = res.name;
+			this.avatarUrl = res.avatar_url;
+		}
+	});
+
+</script>
