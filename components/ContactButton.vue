92838a5acabbb99172b09d6dbe7750c414d66f17
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2022-07-16 14:45:51 +0200

    contact button component

---
+++ components/ContactButton.vue
@@ -0,0 +1,40 @@
+<template>
+	<img
+		class="contact-icon"
+		:src="iconUrl"
+		:alt="altText"
+	>
+</template>
+
+<script lang="ts">
+
+	// Modules
+	import { defineComponent } from "vue";
+
+	export default defineComponent({
+		name: "ContactButton",
+		props: {
+			service: {
+				type: String,
+				required: true
+			}
+		},
+		computed: {
+			iconUrl (): string {
+				return `/icons/${this.service}.svg`;
+			},
+			altText (): string {
+				return `${this.service} icon`;
+			}
+		}
+	});
+
+</script>
+
+<style lang="scss" scoped>
+
+	.contact-icon {
+		color: #FFF;
+	}
+
+</style>
